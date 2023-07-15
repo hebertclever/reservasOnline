3d79b58db1746b3c00f4d22e183d8fc673a61248
Hebert <hebertclever@gmail.com>
2023-07-15 16:01:55 -0300

    logo feito e posicionado na esquerda com um filtro simples na direita

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPollH } from '@fortawesome/free-solid-svg-icons';
 
 function Header({ setFilter, stays }) {
   // Extrai todos os países dos dados de estadia
@@ -11,16 +13,24 @@ function Header({ setFilter, stays }) {
   return (
     <>
       <nav>
-        
+        <div className="container-logo">
+          <FontAwesomeIcon
+            className="logo"
+            icon={faPlay}
+            rotation={270}
+          ></FontAwesomeIcon>
+          <h1>windbnb</h1>
+        </div>
+
         <input
           type="text"
           placeholder="Search by city..."
           onChange={event => setFilter(event.target.value)}
-        />        
+        />
       </nav>
-      <div className='countries'>
-      <h1>{`Stays in ${uniqueCountries.join(', ')}`}</h1>
-        <p>{`${stays.length} +stays` }</p>
+      <div className="countries">
+        <h1>{`Stays in ${uniqueCountries.join(', ')}`}</h1>
+        <p>{`${stays.length} +stays`}</p>
       </div>
     </>
   );
