9d6f5f5e326b88808c4f602feca239d5f30dc133
Hebert <hebertclever@gmail.com>
2023-07-15 22:18:53 -0300

    fiz uma nova navbar na parte superior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,10 +15,9 @@ function Header({ setFilter, stays }) {
   const [showModal, setShowModal] = useState(false);
   console.log('Estado inicial do modal:', showModal);
 
-
-
   return (
     <>
+    <nav className='navbar-modal' > {showModal && <Modal />}</nav>
       <nav
         onMouseLeave={() => {
           // console.log('Mouse saiu da Header');
@@ -34,28 +33,39 @@ function Header({ setFilter, stays }) {
           <h1>windbnb</h1>
         </div>
 
-        <div className='filter-input'
-        onClick={() => {
-          // console.log('Mouse Entrou na Header');
-          setShowModal(true);
-        }}>
-        <input 
-          className='input-search'
-          type="text"
-          placeholder="Helsinki, Finland"
-          onChange={event => {
-            console.log('O valor do input mudou:', event.target.value);
-            setFilter(event.target.value);
+        <div
+          className="filter-input"
+          onClick={() => {
+            // console.log('Mouse Entrou na Header');
+            setShowModal(true);
           }}
-        />
-        <input className='input-number' type="number" id='quantity' placeholder='Add guests'   name='quantity' min={1} max={20} />
-        <div className='input-search-icon' > <FontAwesomeIcon icon={faSearch} /></div> 
-
-        {showModal && <Modal />}
-
+        >
+          <input
+            className="input-search"
+            type="text"
+            placeholder="Helsinki, Finland"
+            style={{ color: '#333' }}
+            onChange={event => {
+              console.log('O valor do input mudou:', event.target.value);
+              setFilter(event.target.value);
+            }}
+          />
+          <input
+            className="input-number"
+            type="number"
+            id="quantity"
+            placeholder="Add guests"
+            name="quantity"
+            min={1}
+            max={20}
+          />
+          <div className="input-search-icon">
+            {' '}
+            <FontAwesomeIcon icon={faSearch} />
+          </div>
         </div>
 
-        
+       
       </nav>
       <div className="countries">
         <h1>{`Stays in ${uniqueCountries.join(', ')}`}</h1>
